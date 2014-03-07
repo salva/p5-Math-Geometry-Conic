c3d468a09535fbe7b0ef2925d4b4e61059b2da98
Salvador Fandino <sfandino@yahoo.com>
2014-03-07 09:53:48 +0100

    this way we always get a set of linear-dependent equations

--- lib/Math/Geometry/Conic.pm
+++ lib/Math/Geometry/Conic.pm
@@ -1,38 +1,67 @@
 package Math::Geometry::Conic;
 
-use 5.018002;
+our $VERSION = '0.01';
+
+use 5.010;
 use strict;
 use warnings;
 
 require Exporter;
+use Math::Vector::Real;
+
 
 our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(point_equidistant_to_three_circunferences);
 
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
+sub _det {
+    my ($x, $y, $z) = @_;
+    my ($x0, $x1, $x2) = @$x;
+    my ($y0, $y1, $y2) = @$y;
+    my ($z0, $z1, $z2) = @$z;
 
-# This allows declaration	use Math::Geometry::Conic ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
+    return (   $x0 * ($y1 * $z2 - $y2 * $z1)
+             + $x1 * ($y2 * $z0 - $y0 * $z2)
+             + $x2 * ($y0 * $z1 - $y1 * $z0) );
+}
 
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+sub point_equidistant_to_three_circunferences {
+    my ($o0, $r0, $o1, $r1, $o2, $r2) = @_;
 
-our @EXPORT = qw(
-	
-);
+    my @o = ($o0, $o1, $o2);
+    my @r = ($r0, $r1, $r2);
 
-our $VERSION = '0.01';
+    my (@X, @Y, @R, @B);
+    for my $i (0..2) {
+        my $j = ($i + 1) % 3;
 
+        my ($xi, $yi) = @{$o[$i]};
+        my $ri = $r[$i];
+        my ($xj, $yj) = @{$o[$j]};
+        my $rj = $r[$j];
 
-# Preloaded methods go here.
+        $X[$i] = $xi - $xj;
+        $Y[$i] = $yi - $yj;
+        $R[$i] = $ri - $rj;
+        $B[$i] = -0.5 * (   $xi * $xi - $xj * $xj
+                          + $yi * $yi - $yj * $yj
+                          - $ri * $ri + $rj * $rj );
+    }
+
+    my $d = _det(\@X, \@Y, \@R);
+    return unless $d;
+    my $k = 1 / $d;
+    my $r = $k * _det(\@X, \@Y, \@B);
+    return unless $r >= 0;
+
+    my $x = $k * _det(\@B, \@Y, \@R);
+    my $y = $k * _det(\@X, \@B, \@R);
+    my $o = V($x, $y);
+    wantarray ? ($o, $r) : $o;
+}
+
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
